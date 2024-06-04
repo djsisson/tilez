05ab15fd40e29ff2cdbb0c4510872eac1fd6eed2
Sarah Wiseman <s.wiseman777@gmail.com>
2024-06-04 10:27:56 +0100

    adding images and instructions to the help modal

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -6,19 +6,46 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import Image from "next/image";
+import blue from "@/assets/blue-tiles.png";
+import green from "@/assets/green-word.png";
+import definition from "@/assets/definition.png";
 
 export default function Help() {
   return (
     <Dialog>
       <DialogTrigger>?</DialogTrigger>
-      <DialogContent>
+      <DialogContent className="w-svw">
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription className="flex flex-wrap">
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </DialogDescription>
+          <DialogTitle className="underline text-center">Rules of the game:</DialogTitle>
         </DialogHeader>
+
+        <DialogDescription className="flex flex-row gap-2">
+          <div className="flex flex-1 flex-col gap-2">
+            <Image src={blue} alt={"tilez blue"} width={300} height={100} />
+            <DialogTitle>
+              Align tiles to spell a word. Selected tiles are blue.
+            </DialogTitle>
+          </div>
+          <div className="flex flex-1 flex-col gap-2">
+            <Image src={green} alt={"tilez green"} width={300} height={100} />
+            <DialogTitle>
+              Tiles where you have found a word turn green.
+            </DialogTitle>
+          </div>
+          <div className="flex flex-1 flex-col gap-2">
+            <Image
+              src={definition}
+              alt={"definition"}
+              width={300}
+              height={100}
+            />
+            <DialogTitle>
+              When you have found a word, hover over the word at the top and you
+              can find the definition!
+            </DialogTitle>
+          </div>
+        </DialogDescription>
       </DialogContent>
     </Dialog>
   );
