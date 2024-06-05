215f7e2169ae3c7d23de7a302c466fcfabb7e4cd
Sarah Wiseman <s.wiseman777@gmail.com>
2024-06-05 10:08:44 +0100

    about page route

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <div className="w-full bg-secondary p-2 text-center">
-      © DJ Sisson et al. Enterprises, 2024.
+      <Link href="/about">© DJ Sisson et al. Enterprises, 2024.</Link>
     </div>
   );
 }
